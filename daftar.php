0312206b39719c656fd3324d8e81f2ae54d940ba
Freksa26 <reksapratama33725@gmail.com>
2024-06-29 07:26:47 +0700

    mengganti icon
    label

--- daftar.php
+++ daftar.php
@@ -67,81 +67,82 @@
 <body background="img/page-background.png">
 
 <div class="container">
-        <form class="form-signin" action="insert-anggota.php" method="post">
-            <div class="text-center">
-                <h2 class="form-signin-heading"><i class="bi bi-book"></i> PerpustakaanPGT</h2>
-            </div>
+    <form class="form-signin" action="insert-anggota.php" method="post">
+        <div class="text-center">
+            <h2 class="form-signin-heading"><i class="bi bi-book"></i> PerpustakaanPGT</h2>
+        </div>
 
-            <div class="form-group">
-                <label for="no_induk">Email</label>
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="bi bi-envelope-open"></i></span>
-                    <input type="text" id="no_induk" name="no_induk" class="form-control" placeholder="Email" autocomplete="off" autofocus="on" required>
-                </div>
+        <div class="form-group">
+            <label for="no_induk">Email</label>
+            <div class="input-group">
+                <span class="input-group-addon right-icon"><i class="bi bi-envelope-open"></i></span>
+                <input type="text" id="no_induk" name="no_induk" class="form-control" placeholder="Email" autocomplete="off" autofocus="on" required>
             </div>
+        </div>
 
-            <div class="form-group">
-                <label for="nama">Nama</label>
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                    <input type="text" id="nama" name="nama" class="form-control" placeholder="Nama" autocomplete="off" autofocus="on" required>
-                </div>
+        <div class="form-group">
+            <label for="nama">Full Name</label>
+            <div class="input-group">
+                <span class="input-group-addon right-icon"><i class="bi bi-person"></i></span>
+                <input type="text" id="nama" name="nama" class="form-control" placeholder="Full Name" autocomplete="off" autofocus="on" required>
             </div>
+        </div>
 
-            <div class="form-group">
-                <label for="username">Username</label>
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                    <input type="text" id="username" name="username" class="form-control" placeholder="Username" autocomplete="off" autofocus="on" required>
-                </div>
+        <div class="form-group">
+            <label for="username">Username</label>
+            <div class="input-group">
+                <span class="input-group-addon right-icon"><i class="bi bi-person"></i></span>
+                <input type="text" id="username" name="username" class="form-control" placeholder="Username" autocomplete="off" autofocus="on" required>
             </div>
+        </div>
 
-            <div class="form-group">
-                <label for="password">Password</label>
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                    <input type="password" id="password" name="password" class="form-control" placeholder="Password" autocomplete="off" required>
-                </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <div class="input-group">
+                <span class="input-group-addon right-icon small-icon"><i class="bi bi-lock"></i></span>
+                <input type="password" id="password" name="password" class="form-control" placeholder="Password" autocomplete="off" required>
             </div>
+        </div>
 
-            <div class="form-group">
-                <label>Jenis Kelamin</label>
-                <div class="radio">
-                    <label><input type="radio" name="jk" value="L"> Laki-laki <i class="bi bi-gender-male"></i></label>
-                    <label><input type="radio" name="jk" value="P"> Perempuan <i class="bi bi-gender-female"></i></label>
-                </div>
+        <div class="form-group">
+            <label>Jenis Kelamin</label>
+            <div class="radio">
+                <label><input type="radio" name="jk" value="L"> Laki-laki <i class="bi bi-gender-male"></i></label>
+                <label><input type="radio" name="jk" value="P"> Perempuan <i class="bi bi-gender-female"></i></label>
             </div>
+        </div>
 
-            <div class="form-group">
-                <label for="kelas">Usia</label>
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                    <input type="text" id="kelas" name="kelas" class="form-control" placeholder="Usia" autocomplete="off" autofocus="on" required>
-                </div>
+        <div class="form-group">
+            <label for="kelas">Usia</label>
+            <div class="input-group">
+                <span class="input-group-addon right-icon"><i class="bi bi-hourglass-split"></i></span>
+                <input type="text" id="kelas" name="kelas" class="form-control" placeholder="Usia" autocomplete="off" autofocus="on" required>
             </div>
+        </div>
 
-            <div class="form-group">
-                <label for="ttl">Tempat, Tanggal Lahir (DD MM YY)</label>
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                    <input type="text" id="ttl" name="ttl" class="form-control" placeholder="Tempat, Tanggal Lahir (DD MM YY)" autocomplete="off" autofocus="on" required>
-                </div>
+        <div class="form-group">
+            <label for="ttl">Tempat, Tanggal Lahir (DD MM YY)</label>
+            <div class="input-group">
+                <span class="input-group-addon right-icon"><i class="bi bi-calendar4-week"></i></i></span>
+                <input type="text" id="ttl" name="ttl" class="form-control" placeholder="Tempat, Tanggal Lahir (DD MM YY)" autocomplete="off" autofocus="on" required>
             </div>
+        </div>
 
-            <div class="form-group">
-                <label for="alamat">Alamat</label>
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                    <input type="text" id="alamat" name="alamat" class="form-control" placeholder="Alamat" autocomplete="off" autofocus="on" required>
-                </div>
+        <div class="form-group">
+            <label for="alamat">Alamat</label>
+            <div class="input-group">
+                <span class="input-group-addon right-icon"><i class="bi bi-house"></i></span>
+                <input type="text" id="alamat" name="alamat" class="form-control" placeholder="Alamat" autocomplete="off" autofocus="on" required>
             </div>
+        </div>
 
-            <br>
-            <input type="submit" value="Daftar" class="btn btn-sm btn-primary">&nbsp;
-            <a href="login.html" class="btn btn-sm btn-danger">Batal</a>
-            <p>Login? <a href="login.html">Klik disini</a></p>
-        </form>
-    </div>
+        <br>
+        <input type="submit" value="Daftar" class="btn btn-sm btn-primary">&nbsp;
+        <a href="login.html" class="btn btn-sm btn-danger">Batal</a>
+        <p>Login? <a href="login.html">Klik disini</a></p>
+    </form>
+</div>
+
  <!-- /container -->
 
   

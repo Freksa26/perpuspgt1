67fde6bd2d6ec5565670be3ebfee5c26a4460cf7
Fikyrie3 <fikriainurridho667@gmail.com>
2024-06-29 12:39:53 +0700

    merubah icon
    merubah icon di menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -1,63 +1,67 @@
-                            <ul class="sidebar-menu">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css">
+
+                           
+                           
+                           <ul class="sidebar-menu">
                                 <li class="active">
                                     <a href="index.php">
-                                        <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                                    <i class="bi bi-house"></i> <span> Beranda </span>
                                     </a>
                                 </li>
 
                                 <li class="treeview">
                                     <a href="#">
-                                        <i class="fa fa-user"></i>
+                                    <i class="bi bi-person"></i> 
                                         <span>Data Anggota</span>
-                                        <i class="fa fa-angle-left pull-right"></i>
+                                         
                                     </a>
                                     <ul class="treeview-menu">
-                                        <li><a href="anggota.php"><i class="fa fa-angle-double-right"></i> Data Anggota</a></li>
-                                        <li><a href="input-anggota.php"><i class="fa fa-angle-double-right"></i> Tambah Anggota</a></li>
+                                        <li><a href="anggota.php">  <i class="bi bi-person"></i>  Data Anggota</a></li>
+                                        <li><a href="input-anggota.php">  <i class="bi bi-person"></i>  Tambah Anggota</a></li>
                                     </ul>
                                 </li>
 
                                 <li class="treeview">
                                     <a href="#">
-                                        <i class="fa fa-book"></i>
+                                    <i class="bi bi-book"></i>
                                         <span>Data Buku</span>
-                                        <i class="fa fa-angle-left pull-right"></i>
+                                        
                                     </a>
                                     <ul class="treeview-menu">
-                                        <li><a href="buku.php"><i class="fa fa-angle-double-right"></i> Data Buku</a></li>
-                                        <li><a href="input-buku.php"><i class="fa fa-angle-double-right"></i> Tambah Buku</a></li>
+                                        <li><a href="buku.php"><i class="bi bi-book"></i> Data Buku</a></li>
+                                        <li><a href="input-buku.php"><i class="bi bi-book"></i> Tambah Buku</a></li>
                                     </ul>
                                 </li>
                                 <li class="treeview">
                                     <a href="#">
-                                        <i class="fa fa-lock"></i>
+                                    <i class="bi bi-lock"></i>
                                         <span>Data Admin</span>
-                                        <i class="fa fa-angle-left pull-right"></i>
+                                        
                                     </a>
                                     <ul class="treeview-menu">
-                                        <li><a href="admin.php"><i class="fa fa-angle-double-right"></i> Data Admin</a></li>
-                                        <li><a href="input-admin.php"><i class="fa fa-angle-double-right"></i> Tambah Admin</a></li>
+                                        <li><a href="admin.php"><i class="bi bi-lock"></i>Data Admin</a></li>
+                                        <li><a href="input-admin.php"><i class="bi bi-lock"></i> Tambah Admin</a></li>
                                     </ul>
                                 </li>
                                 <li class="treeview">
                                     <a href="#">
-                                        <i class="fa fa-file"></i>
+                                    <i class="bi bi-file-earmark"></i>
                                         <span>laporan</span>
-                                        <i class="fa fa-angle-left pull-right"></i>
+                                        
                                     </a>
                                     <ul class="treeview-menu">
-                                        <li><a href="laporan-anggota.php"><i class="fa fa-angle-double-right"></i> Laporan Anggota</a></li>
-                                        <li><a href="laporan-buku.php"><i class="fa fa-angle-double-right"></i> Laporan Buku</a></li>
+                                        <li><a href="laporan-anggota.php"> <i class="bi bi-file-earmark"></i> Laporan Anggota</a></li>
+                                        <li><a href="laporan-buku.php"> <i class="bi bi-file-earmark"></i> Laporan Buku</a></li>
                                     </ul>
                                 </li>
                                 <li>
                                     <a href="backup.php">
-                                        <i class="fa fa-check-square"></i> <span>Backup Data</span>
+                                    <i class="bi bi-file-check"></i> <span>Backup Data</span>
                                     </a>
                                 </li>
                                 <li>
                                     <a href="tentang.php">
-                                        <i class="fa fa-envelope"></i> <span>Tentang PerpustakaanPGT</span>
+                                    <i class="bi bi-envelope"></i> <span>Tentang PerpustakaanPGT</span>
                                     </a>
                                 </li>
 

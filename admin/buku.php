a29835a699fad437cfc9770ee88914df5826945f
Fikyrie3 <fikriainurridho667@gmail.com>
2024-06-29 12:08:18 +0700

    mengganti tampilan tabel buku
    menambah tombol pindah hal dalam tabel, menampilkan 10 buka pada hal buku, dan merubah tampilan agar lbh menarik

--- admin/buku.php
+++ admin/buku.php
@@ -56,7 +56,7 @@ if (empty($_SESSION['username'])) {
         <!-- header logo: style can be found in header.less -->
         <header class="header">
             <a href="index.php" class="logo">
-                PerpustakaanKU
+                PerpustakaanPGT
             </a>
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top" role="navigation">
@@ -153,104 +153,117 @@ if (empty($_SESSION['username'])) {
 
             <!-- Main content -->
             <section class="content">
-
-                <div class="row">
-                    <div class="col-xs-12">
-                        <div class="panel">
-                            <header class="panel-heading">
-                                <b>Data Buku</b>
-
-                            </header>
-                            <!-- <div class="box-header"> -->
-                            <!-- <h3 class="box-title">Responsive Hover Table</h3> -->
-
-                            <!-- </div> -->
-                            <div class="panel-body table-responsive">
-                                <div class="box-tools m-b-15">
-                                    <form action="buku.php" method="POST">
-                                        <div class="input-group">
-                                            <input type='text' class="form-control input-sm pull-right" style="width: 150px;" name='qcari' placeholder='Cari berdasarkan Judul' required />
-                                            <div class="input-group-btn">
-                                                <button class="btn btn-sm btn-default" type="submit"><i class="fa fa-search"></i></button>
-                                            </div>
-                                        </div>
-                                    </form>
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel">
+                <header class="panel-heading">
+                    <b>Data Buku</b>
+                </header>
+                <div class="panel-body table-responsive">
+                    <div class="box-tools m-b-15">
+                        <form action="buku.php" method="POST">
+                            <div class="input-group">
+                                <input type='text' class="form-control input-sm pull-right" style="width: 150px;" name='qcari' placeholder='Cari berdasarkan Judul' required />
+                                <div class="input-group-btn">
+                                    <button class="btn btn-sm btn-default" type="submit"><i class="fa fa-search"></i></button>
                                 </div>
-                                <?php
-                                $query1 = "select * from data_buku";
-
-                                if (isset($_POST['qcari'])) {
-                                    $qcari = $_POST['qcari'];
-                                    $query1 = "SELECT * FROM  data_buku 
-	               where judul like '%$qcari%'
-	               or pengarang like '%$qcari%'  ";
-                                }
-                                $tampil = mysqli_query($conn, $query1) or die(mysqli_error($conn));
-                                ?>
-                                <table id="example" class="table table-hover table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th>
-                                                <center>Judul </center>
-                                            </th>
-                                            <th>
-                                                <center>Pengarang </center>
-                                            </th>
-                                            <th>
-                                                <center>Tahun Terbit </center>
-                                            </th>
-                                            <th>
-                                                <center>Penerbit </center>
-                                            </th>
-                                            <th>
-                                                <center>Jumlah Halaman </center>
-                                            </th>
-                                            <th>
-                                                <center>Tools</center>
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <?php while ($data = mysqli_fetch_array($tampil)) { ?>
-                                        <tbody>
-                                            <tr>
-                                                <td><a href="detail-buku.php?hal=edit&kd=<?php echo $data['id']; ?>"><span class="fa fa-book"></span> <?php echo $data['judul']; ?></a></td>
-                                                <td><?php echo $data['pengarang']; ?></td>
-                                                <td><?php echo $data['th_terbit']; ?></td>
-                                                <td><?php echo $data['penerbit']; ?></td>
-                                                <td><?php echo $data['jumlah_buku']; ?></td>
-                                                <td>
-                                                    <center>
-                                                        <div id="thanks"><a class="btn btn-sm btn-primary" data-placement="bottom" data-toggle="tooltip" title="Edit Buku" href="edit-buku.php?hal=edit&kd=<?php echo $data['id']; ?>"><span class="glyphicon glyphicon-edit"></span></a>
-                                                            <a onclick="return confirm ('Yakin hapus <?php echo $data['judul']; ?>.?');" class="btn btn-sm btn-danger tooltips" data-placement="bottom" data-toggle="tooltip" title="Hapus Buku" href="hapus-buku.php?hal=hapus&kd=<?php echo $data['id']; ?>"><span class="glyphicon glyphicon-trash"></a>
-                                                    </center>
-                                                </td>
-                                            </tr>
                             </div>
+                        </form>
+                    </div>
 
-                        <?php
-                                    }
-                        ?>
+                    <?php
+                    // Konfigurasi untuk halaman
+                    $limit = 10; // Jumlah data per halaman
+                    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+                    $start = ($page - 1) * $limit;
+
+                    $query1 = "SELECT * FROM data_buku";
+                    $countQuery = "SELECT COUNT(*) AS count FROM data_buku";
+
+                    if (isset($_POST['qcari'])) {
+                        $qcari = $_POST['qcari'];
+                        $query1 = "SELECT * FROM data_buku 
+                                   WHERE judul LIKE '%$qcari%' OR pengarang LIKE '%$qcari%'";
+                        $countQuery = "SELECT COUNT(*) AS count FROM data_buku 
+                                       WHERE judul LIKE '%$qcari%' OR pengarang LIKE '%$qcari%'";
+                    }
+
+                    // Menghitung jumlah total data
+                    $resultCount = mysqli_query($conn, $countQuery);
+                    $row = mysqli_fetch_assoc($resultCount);
+                    $total = $row['count'];
+
+                    // Query untuk menampilkan data sesuai halaman
+                    $query1 .= " LIMIT $start, $limit";
+                    $tampil = mysqli_query($conn, $query1) or die(mysqli_error($conn));
+                    ?>
+
+                    <table id="example" class="table table-hover table-bordered">
+                        <thead>
+                            <tr>
+                                <th><center>Judul</center></th>
+                                <th><center>Pengarang</center></th>
+                                <th><center>Tahun Terbit</center></th>
+                                <th><center>Penerbit</center></th>
+                                <th><center>Jumlah Halaman</center></th>
+                                <th><center>Tools</center></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while ($data = mysqli_fetch_array($tampil)) { ?>
+                                <tr>
+                                    <td><a href="detail-buku.php?hal=edit&kd=<?php echo $data['id']; ?>"><span class="fa fa-book"></span> <?php echo $data['judul']; ?></a></td>
+                                    <td><?php echo $data['pengarang']; ?></td>
+                                    <td><?php echo $data['th_terbit']; ?></td>
+                                    <td><?php echo $data['penerbit']; ?></td>
+                                    <td><?php echo $data['jumlah_halaman']; ?></td>
+                                    <td>
+                                        <center>
+                                            <div id="thanks">
+                                                <a class="btn btn-sm btn-primary" data-placement="bottom" data-toggle="tooltip" title="Edit Buku" href="edit-buku.php?hal=edit&kd=<?php echo $data['id']; ?>"><span class="glyphicon glyphicon-edit"></span></a>
+                                                <a onclick="return confirm ('Yakin hapus <?php echo $data['judul']; ?>.?');" class="btn btn-sm btn-danger tooltips" data-placement="bottom" data-toggle="tooltip" title="Hapus Buku" href="hapus-buku.php?hal=hapus&kd=<?php echo $data['id']; ?>"><span class="glyphicon glyphicon-trash"></span></a>
+                                            </div>
+                                        </center>
+                                    </td>
+                                </tr>
+                            <?php } ?>
                         </tbody>
-                        </table>
+                    </table>
 
-                        <?php $tampil = mysqli_query($conn, "select * from data_buku order by id");
-                        $buku = mysqli_num_rows($tampil);
-                        ?>
-                        <center>
-                            <h4>Jumlah Buku : <?php echo "$buku"; ?> Pcs </h4>
-                        </center>
+                    <?php
+                    // Menampilkan navigasi halaman
+                    $total_pages = ceil($total / $limit);
 
-                        <div class="text-right" style="margin-top: 10px;">
-                            <a href="buku.php" class="btn btn-sm btn-info">Refresh Buku <i class="fa fa-refresh"></i></a> <a href="input-buku.php" class="btn btn-sm btn-warning">Tambah Buku <i class="fa fa-arrow-circle-right"></i></a>
-                        </div>
-                        </div><!-- /.box-body -->
-                    </div><!-- /.box -->
-                </div>
+                    echo "<ul class='pagination'>";
+                    if ($page > 1) {
+                        echo "<li><a href='buku.php?page=".($page - 1)."'>Previous</a></li>";
+                    }
+
+                    for ($i = 1; $i <= $total_pages; $i++) {
+                        echo "<li ".($page == $i ? "class='active'" : "")."><a href='buku.php?page=".$i."'>$i</a></li>";
+                    }
+
+                    if ($page < $total_pages) {
+                        echo "<li><a href='buku.php?page=".($page + 1)."'>Next</a></li>";
+                    }
+
+                    echo "</ul>";
+                    ?>
+
+                   
+
+                    <div class="text-right" style="margin-top: 10px;">
+                        <a href="buku.php" class="btn btn-sm btn-info">Refresh Buku <i class="fa fa-refresh"></i></a>
+                        <a href="input-buku.php" class="btn btn-sm btn-warning">Tambah Buku <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div><!-- /.box-body -->
+            </div><!-- /.box -->
+        </div>
     </div>
-    <!-- row end -->
-    </section><!-- /.content -->
+</section><!-- /.content -->
+<!-- /.content -->
     <div class="footer-main">
-        Copyright PerpustakaanKU 2021
+        Copyright PerpustakaanPGT2024
     </div>
     </aside><!-- /.right-side -->
 
